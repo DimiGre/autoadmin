cef9e93655b175515e7d0826f3ae5da86b96fdef
DimiGre <40498160+DimiGre@users.noreply.github.com>
2021-10-05 19:48:54 +0300

    Update Info.java

--- src/main/java/admin/Info.java
+++ src/main/java/admin/Info.java
@@ -5,7 +5,7 @@ public class Info {
         MyGroupId = ID;
     }
     public int MyGroupId;
-    public static String AccessToken = "access_token=535bb6a48c95efd6e01fcf4e481102c349b2f5f6173219685a7493214a8f2b882b3388f04271cb0119885&v=5.131";
+    public static String AccessToken = "";
     public static String VKmethod = "https://api.vk.com/method/";
 
 }
